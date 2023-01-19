b07122f3fa5da8f5f89892620b2b4f4fb18ab404
XanderJ08 <107050836+XanderJ08@users.noreply.github.com>
2023-01-19 11:46:56 -0500

    Delete hello_world_test.py

--- hello_world_test.py
+++
@@ -1,7 +0,0 @@
-# m01 Assignment - Collaboration and Github
-# author: Xander Jewell
-# created: 2023-1-19
-# program created to push into github
-
-# Copying the text from the assignment into this program
-print("Hello World")
